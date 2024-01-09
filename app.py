a01fc4ac04c11bf2ce918a6e1652070ecd687165
alunosNET <alunosnet@gmail.com>
2024-01-09 13:05:00 +0000

    Antes de partir o código

--- app.py
+++ app.py
@@ -33,9 +33,44 @@ def aceitar_cookies():
 
 @app.route('/aluno/adicionar',methods=["GET","POST"])
 def aluno_adicionar():
+    ligacao_bd = basedados.criar_conexao("notas.bd")
     #verificar se o método é GET
-    if request.method=="GET":   #TODO: fazer o form e o POST
+    if request.method=="GET":   
         return render_template('aluno/adicionar.html')
+    if request.method=="POST":
+        #validar os dados
+        nome = request.form.get("input_nome")
+        if not nome:
+            return render_template('aluno/adicionar.html',mensagem="Tem de preencher o campo do nome")
+        if len(nome)<3:
+            return render_template('aluno/adicionar.html',mensagem="Nome muito pequeno. Deve ter pelo menos 3 letras")
+        morada = request.form.get("input_morada")
+        cp = request.form.get("input_cp")
+        data_nasc = request.form.get("input_data")
+        email = request.form.get("input_email")
+        #TODO: guardar fotografia e fazer mais validações
+        #adicionar à bd
+        sql = "INSERT INTO Alunos(nome,morada,cp,data_nasc,email) VALUES (?,?,?,?,?)"
+        parametros = (nome,morada,cp,data_nasc,email)
+        basedados.executar_sql(ligacao_bd,sql,parametros)
+        return redirect("/aluno/listar")
+
+@app.route('/aluno/listar')
+def aluno_listar():
+    ligacao_bd = basedados.criar_conexao("notas.bd")
+    sql = "SELECT * FROM Alunos"
+    dados = basedados.consultar_sql(ligacao_bd,sql)
+    return render_template('aluno/listar.html',registos = dados)
+
+#TODO: pesquisar alunos com base no nome
+
+@app.route('/aluno/editar')
+def aluno_editar():
+    pass
+
+@app.route('/aluno/apagar')
+def aluno_apagar():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
